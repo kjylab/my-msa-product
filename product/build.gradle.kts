a4ae536b64ac43979cadfbc4a9324a22b0fd9ad4
kjylab <yupgod@gmail.com>
2026-05-17 21:21:27 +0900

    Update build.gradle.kts

--- product/build.gradle.kts
+++ product/build.gradle.kts
@@ -7,8 +7,10 @@ dependencies {
     implementation("com.github.kjylab:my-msa-common:v1.0.6")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
-
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
 
     runtimeOnly("com.h2database:h2")
 }
