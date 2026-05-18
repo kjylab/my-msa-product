5db84f611aeb4fb1684b11f2f3e76c98092a1ec8
jyupk <yupgod@gmail.com>
2026-05-18 23:33:17 +0900

    feat: add OpenTelemetry tracing with OTLP exporter for Tempo

--- product-service/build.gradle.kts
+++ product-service/build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
 
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
 
