9d1dc396f98f069ebe5f145ed4cdd29670d4aaeb
GitHub Action <action@github.com>
2026-06-11 20:51:02 +0900

    fix: add sonar.projectName to fix display name

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,6 +67,7 @@ sonar {
 	properties {
 		property("sonar.host.url", "https://sonarcloud.io")
 		property("sonar.organization", "kjylab")
+		property("sonar.projectName", "my-msa-product")
 		property("sonar.projectKey", "kjylab_my-msa-product")
 		property("sonar.qualitygate.wait", "false")
 		property("sonar.coverage.jacoco.xmlReportPaths", "**/build/reports/jacoco/test/jacocoTestReport.xml")
